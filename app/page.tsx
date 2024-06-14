136990c9e69c5d79283bc9aec960a60a70a3bd8d
sandiarbaa <sandiarbaa@gmail.com>
2024-06-14 13:54:34 +0700

    delete hero comp

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,7 @@
-import Hero from "@/Components/Hero";
-import Navbar from "@/Components/Navbar";
-import Image from "next/image";
-
 export default function Home() {
   return (
-    <div className="w-full">
-      <Navbar />
-      <Hero />
+    <div>
+      <h1>Homepage</h1>
     </div>
   );
 }
